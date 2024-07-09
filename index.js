d3e2a3132439d079f9a2265a2d0ab061e8a87be6
Jeferson Chagas Silva <jeferson.chagas@salvador.ba.gov.br>
2024-07-09 11:53:00 -0300

    feat: Implementação documentação com swagger.

--- index.js
+++ index.js
@@ -5,26 +5,9 @@ const appCustom = require('./config/index')
 
 //swagger
 const swaggerUI = require('swagger-ui-express')
-const swaggerJsDoc = require('swagger-jsdoc')
+const swaggerJsDoc = require('./swagger.json')
 
-var swaggerDefinition = {
-  info: {
-    title: 'CRUD Atendimentos Swagger',
-    version: '1.0.00',
-    description: 'Documentação de Api - CRUD Atendimento NodeJS'
-  },
-  components: {
-    schemas: require('./schemas.json')
-  }
-}
-
-var options = {
-  swaggerDefinition: swaggerDefinition,
-  apis: ['./routers/*.js']
-}
-
-var swaggerSpec = swaggerJsDoc(options)
-app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec))
+app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerJsDoc))
 //swagger
 
 appCustom(app, express)
